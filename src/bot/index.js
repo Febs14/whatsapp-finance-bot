388c1144c2bb39714a639e56798480b9be4634f5
Febss <149768765+Febs14@users.noreply.github.com>
2025-06-05 01:00:04 +0700

    Update index.js

--- src/bot/index.js
+++ src/bot/index.js
@@ -7,14 +7,11 @@ const client = new Client({
   authStrategy: new LocalAuth({ clientId: SESSION_NAME }),
   puppeteer: {
     headless: true,
+    executablePath: '/usr/bin/google-chrome-stable',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--disable-gpu'
+      '--disable-dev-shm-usage'
     ]
   }
 });
